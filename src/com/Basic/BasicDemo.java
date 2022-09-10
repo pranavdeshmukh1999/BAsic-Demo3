444e7ac3d0fa4479d87a6a32c3c70262cb8429f2
pranavdeshmukh1999 <pranavdeshmukh182@gmail.com>
2022-09-10 23:11:13 +0530

    Added : Adition logic

--- src/com/Basic/BasicDemo.java
+++ src/com/Basic/BasicDemo.java
@@ -6,6 +6,11 @@ public class BasicDemo {
 		
 		System.out.println("Welcome messege");
 
+		int num1 = 10; 
+		int num2 =20;
+		int sum =num1+num2;
+		
+		System.out.println("Sum is : " +sum);
 	}
 
 }
